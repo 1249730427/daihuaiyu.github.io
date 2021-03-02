7013b27cc31760873594c085abd4d46de4eee56e
1249730427 <daihuaiyu@github.com>
2021-03-02 14:04:33 +0800

    java设计模式享元模式

--- spring-study-demo/src/test/java/ApiTest.java
+++ spring-study-demo/src/test/java/ApiTest.java
@@ -376,4 +376,16 @@ public class ApiTest {
         }
     }
 
+    private ActivityController activityController = new ActivityController();
+
+    @Test
+    public void test_queryActivityInfo() throws InterruptedException {
+        for (int idx = 0; idx < 10; idx++) {
+            Long req = 10001L;
+            Activity activity = activityController.queryActivityInfo(req);
+            logger.info("测试结果：{} {}", req, JSON.toJSONString(activity));
+            Thread.sleep(1200);
+        }
+    }
+
 }
